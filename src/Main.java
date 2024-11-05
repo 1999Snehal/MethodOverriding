870f7c01a958985d8d8ad152ef09bdfca0f4d13a
1999Snehal <snehallagad84@gmail.com>
2024-11-05 17:51:27 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+class Animal
+{
+    public void  displayInfo()
+    {
+        System.out.println("Animal");
+    }
+}
+class Dog extends Animal
+{
+    public void displayInfo()
+    {
+        System.out.println("Dog");
+    }
+}
+
+class MethodOverriding
+{
+    public static void main(String args[])
+    {
+        Animal a1 = new Animal();
+        Dog d1 = new Dog();
+        d1.displayInfo();
+        a1.displayInfo();
     }
 }
